069a2fdde04d51432c9213b23ca8c3a109424aad
ogait-ashtar <ogait.desenvolvedor.jr@gmail.com>
2021-11-08 19:55:35 -0300

    novo

--- pages/index.js
+++ pages/index.js
@@ -34,7 +34,17 @@ export default function Home() {
       <main className={styles.main}>
       
              <section className={styles.content}>
-                         <div className={styles.contat}>
+            <div className={styles.figura}>
+            <Image
+        src="/homemetal/bandas.png"
+        width="400px"
+        height="400px"
+        layout="fixed"
+       />
+           </div>
+
+             <div className={styles.contat}>
+      
           <h2>PROCURE SUA BANDA PREFERIDA </h2>
           <form className={styles.form} method="POST" action="./email.php">
           <input
@@ -55,10 +65,10 @@ export default function Home() {
           Get started by editing{" "}
           <code className={styles.code}>pages/index.js</code>
         </p> */}
-         
-  
+         </section>
+         <section className={styles.anuncio}>
           <div className={styles.contat}>
-          <h2>ANUNCIE SUA BANDA CONOSCO </h2>
+          <h2>SEJA NOSSO PARCEIRO - ANUNCIE SUA BANDA CONOSCO </h2>
             <form className={styles.form} method="POST" action="./email.php">
             <input
               className={styles.field}
@@ -121,9 +131,10 @@ export default function Home() {
             </p>
           </a> */}
         </div>
+        
         <section className={styles.content}>
         <div className={styles.cadastro}>
-          <h2>CADASTRE SUA BANDA </h2>
+          <h2 className={styles.cad}>CADASTRE SUA BANDA </h2>
           <form className={styles.form} method="POST" action="./email.php">
             <input
               className={styles.field}
@@ -133,17 +144,24 @@ export default function Home() {
               className={styles.field}
               name="email"
               placeholder="E-mail"></input>
+              <input
+              className={styles.field}
+              name="estilo musical"
+              placeholder="Estilo Musical"></input>
             <textarea
               className={styles.field}
               name="mesage"
               placeholder="Realese e Redes Sociais"></textarea>
-              <h3>ADD FOTOS</h3>
+              <h3>ADICIONE FOTOS DA BANDA</h3>
                <input type="file" id="flimage" name="flimage" accept="image*/"></input><br></br>
             <input type="submit" value="Enviar"></input><br></br>
             
           </form>
         </div>
       </section>
+      
+      
+      
       </main>
 
        <footer className={styles.footer}>
